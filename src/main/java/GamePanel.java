3580485fac59fb70cc22a847f994a8c244416b07
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-09 11:39:47 -0400

    <feat>added directions movement

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -49,31 +49,109 @@ public class GamePanel extends JPanel implements ActionListener {
       }
       g.setColor(Color.red);
       g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+
+      for(int i=0; i< bodyParts;i++){
+          if(i==0){
+              g.setColor(Color.green);
+              g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+          }
+          else{
+              g.setColor(new Color(45,180,0));
+              g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+          }
+      }
     }
     public void newApple(){
         appleX =random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
         appleY =random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
     public void move() {
-
+      for(int i = bodyParts;i>0;i--){
+          x[i]=x[i-1];
+          y[i]=y[i-1];
+        }
+      switch (direction){
+          case 'U':
+              y[0]=y[0]- UNIT_SIZE;
+              break;
+          case 'D':
+              y[0]=y[0]+ UNIT_SIZE;
+              break;
+          case 'L':
+              x[0]=x[0]- UNIT_SIZE;
+              break;
+          case 'R':
+              x[0]=x[0]+ UNIT_SIZE;
+              break;
+      }
     }
-    public void CheckApple(){
+    public void checkApple(){
 
     }
     public void checkCollisions(){
-
+        //collides with body
+     for(int i=bodyParts; i>0;i--){
+         if((x[0]==x[i]) && (y[0]==y[i] )) {
+             running = false;
+         }
+     }
+     //collides with left border
+        if(x[0]<0){
+            running=false;
+        }
+        //collides with right border
+        if(x[0]>SCREEN_WIDTH){
+            running=false;
+        }
+        //collide with top border
+        if(y[0]<0){
+            running=false;
+        }
+        //collide with bottom border
+        if(y[0]>SCREEN_HEIGHT){
+            running=false;
+        }
+        if( !running){
+            timer.stop();
+        }
     }
     public void gameOver(Graphics g){
 
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+     if(running){
+         move();
+         checkApple();
+         checkCollisions();
+     }
+     repaint();
     }
  public class MyKeyAdapter extends KeyAdapter{
       @Override
      public void keyPressed(KeyEvent e){
-
+      switch (e.getKeyCode()){
+          case KeyEvent.VK_LEFT:
+              if(direction !='R'){
+                  direction='L';
+              }
+              break;
+          case KeyEvent.VK_RIGHT:
+              if(direction !='L'){
+                  direction='R';
+              }
+              break;
+          case KeyEvent.VK_UP:
+              if(direction !='D'){
+                  direction='U';
+              }
+              break;
+          case KeyEvent.VK_DOWN:
+              if(direction !='U'){
+                  direction='D';
+              }
+              break;
+      }
       }
 }
 }
