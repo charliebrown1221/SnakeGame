4c32da105779148fa554b053439eb784f6ba8b52
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-09 11:44:28 -0400

    <feat>apple moves randomly + body parts incres when eating aplle + gameover if collide with body or wall

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -86,7 +86,11 @@ public class GamePanel extends JPanel implements ActionListener {
       }
     }
     public void checkApple(){
-
+     if((x[0]==appleX)&&(y[0]==appleY)){
+         bodyParts++;
+         applesEaten++;
+         newApple();
+     }
     }
     public void checkCollisions(){
         //collides with body
