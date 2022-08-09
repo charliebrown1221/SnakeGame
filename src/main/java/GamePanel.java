65b2b846affc4274c7252c0341c5860b3abe96c5
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-09 11:53:44 -0400

    <feat>added game over screen

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -43,23 +43,27 @@ public class GamePanel extends JPanel implements ActionListener {
     draw(g);
     }
     public void draw(Graphics g){
-      for (int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
-          g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
-          g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
-      }
-      g.setColor(Color.red);
-      g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+        if (running) {
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
-      for(int i=0; i< bodyParts;i++){
-          if(i==0){
-              g.setColor(Color.green);
-              g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
-          }
-          else{
-              g.setColor(new Color(45,180,0));
-              g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
-          }
-      }
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+        }
+        else{
+            gameOver(g);
+        }
     }
     public void newApple(){
         appleX =random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
@@ -120,7 +124,10 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
     public void gameOver(Graphics g){
-
+     g.setColor(Color.red);
+     g.setFont(new Font("Ink Free",Font.BOLD, 75));
+     FontMetrics metrics = getFontMetrics(g.getFont());
+     g.drawString("GAME OVER",(SCREEN_WIDTH - metrics.stringWidth("GAME OVER"))/2,SCREEN_HEIGHT/2);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
